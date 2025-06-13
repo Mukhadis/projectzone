7024e500708493a043e340649205727968c4d8eb
mukhadis yaru <mukhadis.yaru@gmail.com>
2025-06-13 08:10:25 +0100

    added student grade manager, simple programme that tracks the number of pupils in a class, their names and grade

---
+++ student_grade_manager.py
@@ -0,0 +1,38 @@
+# This is a simple programme that records student names and their associated grades, aswell as the class average
+school_class = []
+
+def student_tracker():
+    total = 0
+    count = 0
+
+    # Takes in the size of the call
+    size = input("How many students are in the class? ")
+    print("\n")
+
+    while count < int(size):
+        names = input(f"What is the name of the student {count + 1}? ")
+        grade = int(input("What was their grade? "))
+        print("\n")
+        school_class.append({"names": names, "grade" : grade})
+        count = count + 1
+
+    for e in school_class:
+        print(f"{e['names']} - {e['grade']}")
+
+    for e in school_class:
+        total += e['grade']
+        average_grade = total/ count
+    print("the average grade is ", average_grade)
+
+if __name__ == "__main__":
+
+    while True:
+        decision = input("Would you like to record grades? Yes or No ")
+
+        if decision == "Yes":
+            student_tracker()
+        elif decision == "No":
+            print("Thanks")
+            break
+        else:
+            print("That is not a valid input, please type 'Yes' or 'No'")
